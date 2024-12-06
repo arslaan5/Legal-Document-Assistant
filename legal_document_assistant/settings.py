407fd001a4512283294d96f45023d0c116c1db2f
Arslaan Siddiqui <arslaansiddiqui23@gmail.com>
2024-12-06 17:16:25 +0530

    production 005

--- legal_document_assistant/settings.py
+++ legal_document_assistant/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG")
 
-ALLOWED_HOSTS = ['.railway.app', '127.0.0.1']
+ALLOWED_HOSTS = ['.railway.app', '127.0.0.1', 'lexiassist.onrender.com']
 
 
 # Application definition
